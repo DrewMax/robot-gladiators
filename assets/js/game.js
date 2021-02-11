3ce89a35ced190f69c1eb1c761d98b92f6ed31c5
Drew Maxwell <ajmaxwell.reviresco@gmail.com>
2021-02-11 16:14:45 +0000

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,15 +32,16 @@ var fight = function(enemyName) {
  if (confirmSkip) {
          window.alert(playerName + " has chosen to skip the fight. Goodbye!");
          //subtract money from player money
-     playerMoney = playerMoney - 10;
+     playerMoney = Math.max(0, playerMoney - 10);
      console.log("playerMoney", playerMoney);
      break;
  }
  }
 
  // remove enemy's health by subtracting the amount set in the playerAttack variable
-    // if (promptFight === "fight" || promptFight === "FIGHT") {
-        enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
         );
@@ -55,8 +56,12 @@ var fight = function(enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
    
-    // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    
+// remove player's health by subtracting the amount set in the enemyAttack variable
+
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage)
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -129,7 +134,7 @@ switch (shopOptionPrompt) {
       
             var pickedEnemyName = enemyNames[i];
       
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
       
             fight(pickedEnemyName);
             if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -166,6 +171,13 @@ switch (shopOptionPrompt) {
                 window.alert("Thank you for playing Robot Gladiators! Come back soon!")
             }
           }
+
+          // function to generate a random numeric value
+          var randomNumber = function(min, max) {
+            var value = Math.floor(Math.random() * (max - min + 1) + min);
+          
+            return value;
+          };
 
 // var shop = () => {
 //    console.log("entered the shop");
